6826647e15777148598aecf896234c777395dc8c
SiputBiru <hillsforrest03@gmail.com>
2026-02-05 15:43:07 +0700

    adding somthing to the command buffer so for linux user can see the window

--- src/main.c
+++ src/main.c
@@ -1,35 +1,54 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_keycode.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    // Initialize SDL
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return 1;
     }
 
-    // Create a window
-    SDL_Window *window = SDL_CreateWindow("SDL3 Setup", 800, 600, 0);
+    SDL_Window *window =
+        SDL_CreateWindow("SDL3 Hyprland Test", 800, 600, SDL_WINDOW_RESIZABLE);
     if (!window) {
         SDL_Log("Could not create window: %s", SDL_GetError());
         SDL_Quit();
         return 1;
     }
 
-    SDL_Log("Hello from SDL3!");
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer) {
+        SDL_Log("Could not create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
 
-    // Simple event loop to keep the window open
+    SDL_Log("Window and Renderer created. Starting loop...");
+
     SDL_Event event;
     int running = 1;
+
     while (running) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
                 running = 0;
+            } else if (event.type == SDL_EVENT_KEY_DOWN) {
+                // will close when pressing ESCAPE button
+                if (event.key.key == SDLK_ESCAPE) {
+                    running = 0;
+                }
             }
         }
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
     }
 
-    // Cleanup
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
